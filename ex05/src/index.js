a2071afab105fe154d4ba8d41705e524617757a2
tariktarik <tarik.bajric@gmail.com>
2021-03-12 00:58:45 +0100

    Multiplying two decimals

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // only change code below this line
+    var product;
+
+    product = numOne * numTwo;
+    // Only change code above this line
+    return product;
+
+}
+
+console.log(main(2.5, 2.0));
+
+module.exports = main;
