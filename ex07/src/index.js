b3ffe3995e0ca6976f5862a0ace510d33bdbabba
tariktarik <tarik.bajric@gmail.com>
2021-03-12 01:09:49 +0100

    Finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+    // only change code below this line
+    var remainder;
+  
+    remainder = numOne % numTwo;
+    // Only change code above this line
+  
+    return remainder;
+  }
+  
+  console.log(main(5, 2)); // change this line
+  
+  module.exports = main;
